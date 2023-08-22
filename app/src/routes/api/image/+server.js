1e9b7bbd738b67e96e7d48bccaf91e5a3c97300f
Federico Cappellotto | AQuest <federico.cappellotto@aquest.it>
2023-08-22 17:00:08 +0200

    ✨ Image endpoint with Dall-E

--- app/src/routes/api/image/+server.js
+++ app/src/routes/api/image/+server.js
@@ -1,44 +1,68 @@
+// @ts-nocheck
 import { error } from '@sveltejs/kit';
 
 // Svelte Component To Image
 import { image_from_component } from 'svelte-component-to-image';
-
+import OpenAI from 'openai';
 // Normal .svelte component
 import Image from './Image.svelte';
-/* 
+import { OPENAI_API_KEY } from '$env/static/private';
+import fs from 'fs';
+
 const openai = new OpenAI({
 	apiKey: OPENAI_API_KEY
 });
 
-async function dalle() {
+async function dalle(prompt) {
+	if (!prompt) {
+		return null;
+	}
 	try {
-		const image = await openai.images.generate({
-			prompt: 'a white siamese cat',
-			n: 1,
-			size: '256x256'
-		});
-
-		console.log(image.data);
+		const path = `./dalle/${prompt}.png`;
+		if (fs.existsSync(path)) {
+			const img = fs.readFileSync(path);
+			return Buffer.from(img).toString('base64');
+		} else {
+			const image = await openai.images.generate({
+				prompt,
+				n: 1,
+				size: '256x256',
+				response_format: 'b64_json'
+			});
+			if (!image.data?.[0]?.b64_json) {
+				return;
+			}
+			fs.writeFile(path, image.data[0].b64_json, 'base64', (err) => {
+				if (err) throw err;
+			});
+			return image.data?.[0]?.b64_json;
+		}
 	} catch (e) {
 		console.error(e);
-		throw error(500, 'Error trying to generate image from component.');
+		return null;
 	}
-} */
+}
 
 export const GET = async ({ url }) => {
 	try {
-		// @ts-ignore
 		const width = url?.searchParams?.get('width') ? parseInt(url.searchParams.get('width')) : 1;
-		// @ts-ignore
 		const height = url?.searchParams?.get('height') ? parseInt(url.searchParams.get('height')) : 1;
+		const prompt = url?.searchParams?.get('prompt') ? url.searchParams.get('prompt') : null;
+
+		const dalleImg = await dalle(prompt);
+
 		const options = {
 			width,
-			// @ts-ignore
 			height,
 			props: {
-				text: url.searchParams.get('text') || `${width}x${height}`,
+				text:
+					url.searchParams.get('text') == 'default'
+						? `${width}x${height}`
+						: url.searchParams.get('text'),
 				bgColor: url.searchParams.get('bgColor') || 'FFFFFF',
-				textColor: url.searchParams.get('textColor') || 'FFFFFF'
+				textColor: url.searchParams.get('textColor') || 'FFFFFF',
+				img: dalleImg,
+				width
 			},
 			fonts: [
 				{
--- app/src/routes/api/image/Image.svelte
+++ app/src/routes/api/image/Image.svelte
@@ -2,6 +2,8 @@
 	export let text = '';
 	export let bgColor = 'FFFFFF';
 	export let textColor = '000000';
+	export let img = '';
+	export let width = 1;
 </script>
 
 <div
@@ -11,8 +13,22 @@
 		? bgColor
 		: 'FFFFFF'}; width: 100%; height: 100%"
 >
+	{#if img}
+		<img
+			src={`data:image/png;base64,${img}`}
+			width="1024"
+			height="1024"
+			alt=""
+			style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover"
+		/>
+	{/if}
+
 	{#if text}
-		<h1 style="color: #{/^[0-9A-F]{6}$/i.test(textColor) ? textColor : '000000'}; font-size: 200px">
+		<h1
+			style="color: #{/^[0-9A-F]{6}$/i.test(textColor) ? textColor : '000000'}; font-size: {(width *
+				100) /
+				1024}px"
+		>
 			{text}
 		</h1>
 	{/if}
